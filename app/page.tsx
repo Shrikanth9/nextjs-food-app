f96f0dd268b57e7f7abc2209ee6af190ab1a13a9
Shrikanth <shrikanthama.vc@flipkart.com>
2024-09-16 17:08:17 +0530

    added main page contents

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,51 @@
+import ImageSlideshow from "@/components/images/image-slideshow";
 import Link from "next/link";
-
+import styles from "./page.module.css";
 export default function Home() {
-  return (
-     <main>
-        <h1 style={{ textAlign: "center", color: "white"}}> Welcome to food app </h1>
-        <p><Link href="/meals">Meals</Link></p>
-        <p><Link href="/community">Community</Link></p>
-        <p><Link href="/meals/share">Share</Link></p>
-     </main>
-  );
+   return (
+      <>
+         <header className={styles.header}>
+            <div className="slideshow">
+            <ImageSlideshow />
+            </div>
+            <div>
+               <div className={styles.hero}>
+                  <h1>Food for foodies</h1>
+                  <p>Find the best recipes</p>
+               </div>
+               <div className={styles.cta}>
+                  <Link href="/community"> Join the community </Link>
+                  <Link href="/meals"> Explore the meals </Link>
+               </div>
+            </div>
+         </header>
+         <main>
+            <section className={styles.section}>
+               <h2>How it works</h2>
+               <p>
+                  NextLevel Food is a platform for foodies to share their favorite
+                  recipes with the world. It&apos;s a place to discover new dishes, and to
+                  connect with other food lovers.
+               </p>
+               <p>
+                  NextLevel Food is a place to discover new dishes, and to connect
+                  with other food lovers.
+               </p>
+            </section>
+
+            <section className={styles.section}>
+               <h2>Why NextLevel Food?</h2>
+               <p>
+                  NextLevel Food is a platform for foodies to share their favorite
+                  recipes with the world. It&apos;s a place to discover new dishes, and to
+                  connect with other food lovers.
+               </p>
+               <p>
+                  NextLevel Food is a place to discover new dishes, and to connect
+                  with other food lovers.
+               </p>
+            </section>
+         </main>
+      </>
+   );
 }
--- components/main-header/main-header.tsx
+++ components/main-header/main-header.tsx
@@ -1,15 +1,20 @@
 import logoImg from "@/assets/logo.png";
+import styles from "@/components/main-header/main-header.module.css";
+import Image from "next/image";
 import Link from "next/link";
+import MainHeaderBackground from "./main-header-bg";
 
 export default function MainHeader() {
     return (
-        <header>
-            <Link href="/">
-               <img src={logoImg.src} alt="A plate with food on it" />
-                  NextLevel food
+        <>
+        <MainHeaderBackground />
+        <header className={styles.header}>
+            <Link href="/" className={styles.logo}>
+               <Image className={styles.logo} src={logoImg} alt="A plate with food on it" priority/>
+                  Food app
             </Link>
 
-           <nav>
+           <nav className={styles.nav}>
             <ul>
                 <li>
                     <Link href="/meals">Meals</Link>
@@ -23,6 +28,7 @@ export default function MainHeader() {
             </ul>
         </nav>
         </header>
+        </>
 
     );
 }
